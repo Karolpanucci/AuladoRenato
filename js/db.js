60ff99d0a2cc813f96a00b3675ac48167bae9940
karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-09-24 16:54:29 -0400

    pronto

--- js/db.js
+++ js/db.js
@@ -28,7 +28,9 @@ window.addEventListener("DOMContentLoaded", async event => {
     createDB();
     document.getElementById("btnSalvar").addEventListener("click", addData);
     document.getElementById("btnListar").addEventListener("click", getData);
-});
+    document.getElementById("btnBuscar").addEventListener("click", buscar);
+    document.getElementById("btnRemover").addEventListener("click", remover);
+    document.getElementById("btnAtualizar").addEventListener("click", atualizar);})
 
 async function getData() {
     if (db == undefined) {
@@ -71,19 +73,56 @@ function showResult(text) {
     document.querySelector("output").innerHTML = text;
 }
 
-async function buscar(){
+async function buscar() {
     const nomeBuscando = document.getElementById('buscarNome').value;
     const tx = db.transaction('pessoas', 'readonly');
-    const store = tx.objectStore ('pessoas');
-     try{
+    const store = tx.objectStore('pessoas');
+    try {
         let objetoBuscado = await store.get(nomeBuscando);
-        document.getElementById('nome').value =
-        objetoBuscado.nome
-        document.getElementById('idade').value =
-        objetoBuscado.idade
-     } catch(error){
+        if (objetoBuscado) {
+            document.getElementById('nome').value = objetoBuscado.nome;
+            document.getElementById('idade').value = objetoBuscado.idade;
+        } else {
+            showResult(`Nenhum registro encontrado para o nome ${nomeBuscando}.`);
+        }
+    } catch (error) {
         console.log(error.message);
-     }
+        showResult("Erro ao buscar dados.");
+    }
 }
 
+async function remover() {
+    const nomeParaRemover = document.getElementById('nome').value;
+    const tx = db.transaction('pessoas', 'readwrite');
+    const store = tx.objectStore('pessoas');
+    try {
+        await store.delete(nomeParaRemover);
+        showResult(`Registro para o nome ${nomeParaRemover} removido.`);
+        document.getElementById('nome').value = "";
+        document.getElementById('idade').value = "";
+    } catch (error) {
+        console.log(error.message);
+        showResult("Erro ao remover o registro.");
+    }
+}
+
+async function atualizar() {
+    const nomeParaAtualizar = document.getElementById('nome').value;
+    const novaIdade = document.getElementById('idade').value;
+    const tx = db.transaction('pessoas', 'readwrite');
+    const store = tx.objectStore('pessoas');
+    try {
+        let objetoExistente = await store.get(nomeParaAtualizar);
+        if (objetoExistente) {
+            objetoExistente.idade = novaIdade;
+            await store.put(objetoExistente);
+            showResult(`Registro para o nome ${nomeParaAtualizar} atualizado com a nova idade ${novaIdade}.`);
+        } else {
+            showResult(`Nenhum registro encontrado para o nome ${nomeParaAtualizar}.`);
+        }
+    } catch (error) {
+        console.log(error.message);
+        showResult("Erro ao atualizar o registro.");
+    }
+}
 
